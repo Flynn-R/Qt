04808082642924809055584c1f1037d142c1f8a0
pierre <pierre@dunn.myguest.virtualbox.org>
2023-02-20 19:55:47 +0300

    Homework-7 ongoing

--- Lesson 7/Homework_7/Task_2/Task_2/mainwindow.cpp
+++ Lesson 7/Homework_7/Task_2/Task_2/mainwindow.cpp
@@ -1,14 +1,91 @@
 #include "mainwindow.h"
+#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow{parent}
+    : QGraphicsView{parent}
 {
     scene = new QGraphicsScene(this);
     setScene(scene);
+}
+
+void MainWindow::addBlock(BlockScheme* item)
+{
+    scene->addItem(item);
+    connect(item, &BlockScheme::addBlock, this, &MainWindow::addBlock);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent* event)
+{
+    auto item = new BlockScheme(event->position().toPoint(), this);
+    addBlock(item);
+}
+
+BlockScheme::BlockScheme(QPoint pos, QObject* parent, double multi) : QObject(parent), QGraphicsItem(), x(pos.x()), y(pos.y()), multiplier(multi)
+{
+    srand(clock());
+    brush.setColor(QColor(rand() % 256, rand() % 256, rand() % 256));
+    brush.setStyle(Qt::SolidPattern);
+    if (figureNext == RECTANGLE)
+    {
+        figure = RECTANGLE;
+        figureNext = ELLIPSE;
+    }
+    else if (figureNext == ELLIPSE)
+    {
+        figure = ELLIPSE;
+        figureNext = STAR;
+    }
+    else
+    {
+        figure = STAR;
+        figureNext = RECTANGLE;
+    }
+    setPos(x, y);
+    moving = false;
+}
+
+void BlockScheme::paint(QPainter* painter, const QStyleOptionGraphicsItem* options, QWidget* widget)
+{
+    Q_UNUSED(options)
+    Q_UNUSED(widget)
+    painter->setBrush(brush);
+    if (figure == RECTANGLE)
+        painter->drawRect(x, y, 200 * multiplier, 100 * multiplier);
+    else if (figure == ELLIPSE)
+        painter->drawEllipse(x, y, 200 * multiplier, 100 * multiplier);
+    else
+    {
+        QList<QPointF> poly;
+        poly.append(QPointF(x, 80 * multiplier));
+        poly.append(QPointF(x + 18 * multiplier, y + 26 * multiplier));
+        poly.append(QPointF(x + 80 * multiplier, y + 26 * multiplier));
+        poly.append(QPointF(x + 30 * multiplier, y - 10 * multiplier));
+        poly.append(QPointF(x + 50 * multiplier, y - 65 * multiplier));
+        poly.append(QPointF(x, y - 32 * multiplier));
+        poly.append(QPointF(x - 50 * multiplier, y - 68 * multiplier));
+        poly.append(QPointF(x - 30 * multiplier, y - 10 * multiplier));
+        poly.append(QPointF(x - 80 * multiplier, y + 26 * multiplier));
+        poly.append(QPointF(x - 18 * multiplier, y + 26 * multiplier));
+        QPolygonF star(poly);
+        painter->drawPolygon(star);
+    }
 }
 
 void BlockScheme::setBrush(QBrush br)
 {
     brush = br;
     emit redraw();
+}
+
+QRectF BlockScheme::boundingRect() const
+{
+    return QRectF(x, y, 200 * multiplier, 100 * multiplier);
+}
+
+void BlockScheme::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+    if (event->button() == Qt::RightButton)
+        emit deleteBlock();
+    if (event->button() == Qt::LeftButton)
+        emit addBlock(event);
 }
--- Lesson 7/Homework_7/Task_2/Task_2/mainwindow.h
+++ Lesson 7/Homework_7/Task_2/Task_2/mainwindow.h
@@ -5,20 +5,28 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QObject>
+#include <QGraphicsItem>
+#include <QMouseEvent>
+#include <QGraphicsSceneMouseEvent>
+
+class BlockScheme;
 
 class MainWindow : public QGraphicsView
 {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ~MainWindow() = default;
 
 private:
     QGraphicsView* view;
     QGraphicsScene* scene;
 
-signals:
+private slots:
+    void addBlock(BlockScheme*);
 
+protected:
+    void mousePressEvent(QMouseEvent*) override;
 };
 
 class BlockScheme : public QObject, public QGraphicsItem
@@ -26,12 +34,33 @@ class BlockScheme : public QObject, public QGraphicsItem
     Q_OBJECT
 
 public:
-    explicit BlockScheme(QObject* parent = nullptr);
+    explicit BlockScheme(QPoint, QObject* parent = nullptr, double multi = 1.0);
     void setBrush(QBrush);
 
 private:
-    void paint(QPainter*);
+    void paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*) override;
+    QRectF boundingRect() const override;
+    qint32 x, y;
+    enum Geometry
+    {
+        RECTANGLE, ELLIPSE, STAR
+    };
+    static Geometry figureNext;
+    Geometry figure;
+    QPoint beginPoint;
+    bool moving;
+    QBrush brush;
+    double multiplier;
 
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent*) override;
+//    void mouseReleaseEvent(QGraphicsSceneMouseEvent*) override;
+//    void mouseMoveEvent(QGraphicsSceneMouseEvent*) override;
+
+signals:
+    void redraw();
+    void addBlock(QGraphicsSceneMouseEvent*);
+    void deleteBlock();
 };
 
 #endif // MAINWINDOW_H

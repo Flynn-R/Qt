885c4a6fe9b9e6510b6b154920d7ab4866653724
Flynn <seyfullin.contact@icloud.com>
2023-02-22 14:33:55 +0300

    copy files from previous project version

---
+++ Lesson 8/Text_Editor/dialog.h
@@ -0,0 +1,43 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+    void setKeyMap(QMap<const QString, QKeySequence>);
+    void setLanguageRadioButton(bool);
+    void setTheme(bool);
+    void translate();
+    ~Dialog();
+
+private slots:
+    void on_closeSeq_editingFinished();
+    void on_saveSeq_editingFinished();
+    void on_openSeq_editingFinished();
+    void on_helpSeq_editingFinished();
+    void on_newSeq_editingFinished();
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
+    void on_readOnlySeq_editingFinished();
+
+signals:
+    void hotkeyChanged(QMap<const QString, QKeySequence>);
+    void languageSelected(bool);
+    void themeSelected(bool);
+
+private:
+    Ui::Dialog *ui;
+    QMap<const QString, QKeySequence> newKeyMap;
+    bool finder(QKeySequence&);
+};
+
+#endif // DIALOG_H

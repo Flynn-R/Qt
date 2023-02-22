cb01c5ddce2d0e0398002918f645522e449ec211
Flynn <seyfullin.contact@icloud.com>
2023-02-22 14:33:55 +0300

    copy files from previous project version

---
+++ Lesson 8/Text_Editor/QtLanguage_ru.ts
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="dialog.ui" line="20"/>
+        <location filename="dialog.cpp" line="129"/>
+        <source>Настройки</source>
+        <oldsource>Горячие клавиши</oldsource>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="41"/>
+        <location filename="dialog.cpp" line="141"/>
+        <source>Тема</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="61"/>
+        <location filename="dialog.cpp" line="142"/>
+        <source>Светлая</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="82"/>
+        <location filename="dialog.cpp" line="143"/>
+        <source>Тёмная</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="195"/>
+        <location filename="dialog.cpp" line="131"/>
+        <source>Горячие клавиши</source>
+        <translation>Горячие клавиши</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="206"/>
+        <location filename="dialog.cpp" line="132"/>
+        <source>Закрыть</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="213"/>
+        <location filename="dialog.cpp" line="133"/>
+        <source>Сохранить</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="220"/>
+        <location filename="dialog.cpp" line="134"/>
+        <source>Открыть</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="227"/>
+        <location filename="dialog.cpp" line="135"/>
+        <source>Открыть для чтения</source>
+        <translation>Открыть для чтения</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="234"/>
+        <location filename="dialog.cpp" line="136"/>
+        <source>Справка</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="241"/>
+        <location filename="dialog.cpp" line="137"/>
+        <source>Новый</source>
+        <translation>Новый</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="121"/>
+        <location filename="dialog.cpp" line="139"/>
+        <source>Язык</source>
+        <translation>Язык</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <location filename="mainwindow.cpp" line="208"/>
+        <source>Текстовый редактор</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="mainwindow.cpp" line="28"/>
+        <location filename="mainwindow.cpp" line="203"/>
+        <source>Сохранить</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <location filename="mainwindow.cpp" line="25"/>
+        <location filename="mainwindow.cpp" line="26"/>
+        <location filename="mainwindow.cpp" line="204"/>
+        <source>Открыть</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>Новый</source>
+        <translation>Новый</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <location filename="mainwindow.cpp" line="27"/>
+        <location filename="mainwindow.cpp" line="205"/>
+        <source>Открыть для чтения</source>
+        <translation>Открыть для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <location filename="mainwindow.cpp" line="29"/>
+        <location filename="mainwindow.cpp" line="252"/>
+        <source>Печать</source>
+        <translation>Печать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="192"/>
+        <location filename="mainwindow.cpp" line="31"/>
+        <location filename="mainwindow.cpp" line="206"/>
+        <source>Закрыть</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <location filename="mainwindow.cpp" line="207"/>
+        <source>Справка</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <source>Только для чтения</source>
+        <translation type="vanished">Только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="34"/>
+        <location filename="mainwindow.cpp" line="202"/>
+        <source>Настройки</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Файл</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>Тема</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="37"/>
+        <source>Светлая</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>Тёмная</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>Язык</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>Сохранить текстовый файл</source>
+        <translation>Сохранить текстовый файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <source>Открыть текстовый файл</source>
+        <translation>Открыть текстовый файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>Текст (*.txt)</source>
+        <translation>Текст (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>Открыть файл для чтения</source>
+        <translation>Открыть файл для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="140"/>
+        <source>:/help/help (rus).txt</source>
+        <translation>:/help/help (rus).txt</translation>
+    </message>
+</context>
+</TS>

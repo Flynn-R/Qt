9d21625fa69f31423ea4bc5dd4f4c88edda9b1cf
Flynn <seyfullin.contact@icloud.com>
2023-02-27 14:48:20 +0300

    Homework-10 incomplete

---
+++ Lesson 10/Organizer/organizer.h
@@ -0,0 +1,28 @@
+#ifndef ORGANIZER_H
+#define ORGANIZER_H
+
+#include <QObject>
+#include <QFile>
+#include <QDate>
+
+class Organizer : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Organizer(QObject *parent = nullptr);
+    ~Organizer();
+    Q_INVOKABLE void addNewTask(QString, QString, QString);
+    Q_INVOKABLE void writeToFile();
+    Q_INVOKABLE QString currentStatus();
+
+private:
+    QFile* file;
+    QStringList taskList;
+
+signals:
+    void fileLoaded(bool success);
+    void incorrectData();
+};
+
+#endif // ORGANIZER_H
